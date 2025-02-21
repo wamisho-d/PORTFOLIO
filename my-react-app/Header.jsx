ced91608e6ddd020209ff32c8d3c5b225396a1f8
wamisho-d <166142918+wamisho-d@users.noreply.github.com>
2025-02-21 00:46:21 -0500

    Update Header.jsx

---
+++ my-react-app/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img src="https://via.placeholder.com/160" alt="profile picture"></img>
+      <h1 className="name-overlay">Wamisho Debero</h1>
+    </div>
+  );
+};
+
+export default Header;
